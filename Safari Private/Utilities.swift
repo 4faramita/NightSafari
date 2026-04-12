e0e492437dd2b79e1f37e971f786683b2a109018
4faramita <14917258+4faramita@users.noreply.github.com>
2026-04-12 15:22:03 +0900

    Reuse existing window and close Start Page

--- Safari Private/Utilities.swift
+++ Safari Private/Utilities.swift
@@ -2,19 +2,59 @@ import SwiftUI
 
 
 func openPrivateSafariWindow(with urls: [URL]) {
+	guard !urls.isEmpty else { return }
+
+	let firstURL = urls[0].absoluteString
+	let allURLsScript = urls.map { #"open location "\#($0.absoluteString)""# }.joined(separator: "\n\t\t\t")
+	let remainingURLsScript = urls.dropFirst().map { #"open location "\#($0.absoluteString)""# }.joined(separator: "\n\t\t\t")
+
 	runAppleScript(
 		#"""
+		tell application "System Events"
+			set safariRunning to (exists application process "Safari")
+		end tell
+
 		tell application "Safari" to activate
 
-		tell application "System Events" to tell its application process "Safari"
-			set frontmost to true
-			keystroke "n" using {shift down, command down}
+		if safariRunning then
+			delay 0.2
+		else
+			delay 1.0
+		end if
+
+		set foundPrivateWindow to false
+
+		tell application "System Events"
+			tell application process "Safari"
+				set frontmost to true
+				repeat with w in windows
+					if name of w contains "Private Browsing" then
+						perform action "AXRaise" of w
+						set foundPrivateWindow to true
+						exit repeat
+					end if
+				end repeat
+			end tell
 		end tell
 
-		tell application "Safari"
-			delay 0.1
-			\#(urls.map { #"open location "\#($0.absoluteString)""# }.joined(separator: "\n"))
-		end tell
+		if foundPrivateWindow then
+			tell application "Safari"
+				delay 0.1
+				\#(allURLsScript)
+			end tell
+		else
+			tell application "System Events" to tell application process "Safari"
+				set frontmost to true
+				keystroke "n" using {shift down, command down}
+			end tell
+
+			tell application "Safari"
+				delay 0.3
+				set startPageTab to current tab of front window
+				\#(allURLsScript)
+				close startPageTab
+			end tell
+		end if
 		"""#
 	)
 }
